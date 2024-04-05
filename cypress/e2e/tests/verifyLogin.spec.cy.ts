91e150767c03a38b15e25f35792ef0e49b6d47ec
ondrejkrtek1 <ondrej.krtek@actumdigital.com>
2024-04-05 12:40:14 +0200

    Prereview

--- cypress/e2e/tests/verifyLogin.spec.cy.ts
+++ cypress/e2e/tests/verifyLogin.spec.cy.ts
@@ -1,27 +1,27 @@
 import loginPage from "../../pageobjects/loginPages";
 
-describe ('POM Test', () => {
-
-    before
-
-    beforeEach(function() {
-        // executes prior each test within it block
-        cy.visit('https://demo.guru99.com/test/newtours/login.php')
-    })
+describe('POM Test', () => {
+    beforeEach(() => {
+        // Provedení kroku pouze jednou před každým testem
+        cy.visit('https://demo.guru99.com/test/newtours/login.php');
+        //cy.get('#gdpr-consent-notice').should('be.visible')
+        //cy.contains('Accept All').click()
+    });
 
     it('Verify Login successful', () => {
         const loginObj = new loginPage();
-        loginObj.enterUsername('selenium@qa')
-        loginObj.enterPassword('qa@12345')
+        loginObj.enterUsername('selenium@qa');
+        loginObj.enterPassword('qa@12345');
         loginObj.clickSubmit();
         loginObj.elements.successTxt().should('have.text', 'Login Successfully');
-    })
+    });
 
     it('Verify Login unsuccessful for invalid username/password', () => {
         const loginObj = new loginPage();
-        loginObj.enterUsername('selenium')
-        loginObj.enterPassword('qa@123')
+        loginObj.enterUsername('selenium');
+        loginObj.enterPassword('qa@123');
         loginObj.clickSubmit();
         loginObj.elements.errorTxt().should('contain','Enter your userName and password correct');
-    })
-})
+    });
+});
+
