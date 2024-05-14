3996ded0f9b8b05aa1e18cd3edcb272416b521b5
ondrejkrtek1 <ondrej.krtek@actumdigital.com>
2024-05-14 13:21:59 +0200

    Changing structure - landing page

--- cypress/e2e/tests/verifyLandingPage.spec.cy.ts
+++ cypress/e2e/tests/verifyLandingPage.spec.cy.ts
@@ -1,27 +1,32 @@
-import landingPageObjects from "../../pageobjects/landingpage";
+//import landingPageObjects from "../../pageobjects/landingpage";
+import LandingPage from "../../pageobjects/landingpage";
 describe('POM Test', () => {
+
+    const landingPage = new LandingPage()
+
     beforeEach(() => {
         // Provedení kroku pouze jednou před každým testem
         cy.visit('https://www.demoblaze.com/index.html');
         //cy.get('#gdpr-consent-notice').should('be.visible')
         //cy.contains('Accept All').click()
     });
-
-    it('Verify that header is visible', () => {
-        const headercheck = new landingPageObjects.landingPageHeader();
-        headercheck.checkExistHeader();
-    });
-
-    it('Verify that footer is visible', () => {
-        const footercheck = new landingPageObjects.landingPageFooter();
-        footercheck.checkExistFooter();
-    });
-
-    it('Verify logo is present and clickable', () => {
-        const logocheck = new landingPageObjects.landingPageLogo()
-        logocheck.checkExistLogo();
+    
+    it('Verify header is present', () => {
+        landingPage.checkExistHeader()
+        landingPage.elements.headerElement().should('be.visible')
 
     })
+    it('Verify footer is present', () => {
+        landingPage.checkExistFooter()
+        landingPage.elements.footerElement().should('be.visible')
+
+    })
+    it('Verify logo is present and clickable', () => {
+        landingPage.checkExistLogo()
+        landingPage.elements.clickableLogo().should('be.visible')
+
+    })
+
 
 
 });
--- cypress/pageobjects/landingpage.ts
+++ cypress/pageobjects/landingpage.ts
@@ -1,45 +1,23 @@
-class landingPageHeader{
+export default class LandingPage{
 
     elements = {
-        headerElement : () => cy.get('#narvbarx')
+        headerElement : () => cy.get('#narvbarx'),
+        clickableLogo : () => cy.get('#nava'),
+        footerElement : () => cy.get('#footc')
     }
 
     checkExistHeader()
     {
         this.elements.headerElement().should('be.visible')
     }
-}
-class landingPageFooter{
 
-    elements = {
-        footerElement : () => cy.get('#footc')
+    checkExistLogo()
+    {
+        this.elements.clickableLogo().should('be.visible')
     }
-    
     checkExistFooter()
     {
         this.elements.footerElement().should('be.visible')
     }
 }
 
-
-class landingPageLogo{
-
-    elements = {    
-        clickableLogo : () => cy.get('#nava')
-    }
-
-    checkExistLogo()
-    {
-        this.elements.clickableLogo().should('be.visible')
-    }
-}
-
-
-
-const landingPageObjects = {
-    landingPageHeader,
-    landingPageFooter,
-    landingPageLogo
-};
-
-export default landingPageObjects
